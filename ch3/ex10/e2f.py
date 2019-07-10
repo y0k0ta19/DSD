1a7c267c65b061cb2b155a0c3374c53ff92e6a16
y0k0ta19 <motoyk@gmail.com>
2019-07-10 09:59:28 +0900

    add ex08-14

---
+++ ch3/ex10/e2f.py
@@ -0,0 +1,5 @@
+e2f = { "dog" : "chien", "cat" : "chat", "walrus" : "morse"}
+print(e2f)
+
+# result
+# {'dog': 'chien', 'cat': 'chat', 'walrus': 'morse'}
---
+++ ch3/ex14/e2f.py
@@ -0,0 +1,7 @@
+e2f = { "dog" : "chien", "cat" : "chat", "walrus" : "morse"}
+ewords = set(e2f.keys())
+print(ewords)
+
+# result
+# mbpA1989:ex14 y0k0ta19$ python3 e2f.py                                                                                                             [master]
+# {'walrus', 'dog', 'cat'}
